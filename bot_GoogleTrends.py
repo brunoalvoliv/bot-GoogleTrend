95beed9284b23fcebfb56aa164df0f6866f3d68f
Bruno Alves <brunoalvesdeoliveira96@gmail.com>
2021-11-19 22:25:36 -0300

    Finished the script

--- bot_GoogleTrends.py
+++ bot_GoogleTrends.py
@@ -2,13 +2,17 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from pytrends.request import TrendReq
+
+plt.style.use('ggplot')
 
 #Buscando dados
 
 pytrends = TrendReq(hl='en-US', tz=360)
 
-all_keywords = ['Política', 'Bolsonaro']
+all_keywords = ['Samsung', 'Iphone', 'Redmi']
 
 keywords = []
 
@@ -76,7 +80,65 @@ def check_trends():
 
     print('')
 
-for kw in all_keywords:
+'''for kw in all_keywords:
     keywords.append(kw)
     check_trends()
-    keywords.pop()
+    keywords.pop()'''
+
+def comparacao():
+    plt.figure(figsize=(12, 6))
+    x_pos = np.arange(len(all_keywords))
+
+    #Last 5-years
+    pytrends.build_payload(all_keywords, cat, timeframes[0], geo, gprop)
+
+    data =  pytrends.interest_over_time()
+    media = data.mean()
+    media = round(media / media.max() * 100, 2)
+    ax1 = plt.subplot2grid((3, 2), (0, 0), rowspan=1, colspan=1)
+    ax2 = plt.subplot2grid((3, 2), (0, 1), rowspan=1, colspan=1)
+    for kw in all_keywords:
+        ax1.plot(data[kw], label=kw)
+    ax2.bar(x_pos, media, align='center')
+    plt.xticks(x_pos, all_keywords)
+
+    #Last 12-months
+    pytrends.build_payload(all_keywords, cat, timeframes[1], geo, gprop)
+
+    data =  pytrends.interest_over_time()
+    media = data.mean()
+    media = round(media / media.max() * 100, 2)
+    ax3 = plt.subplot2grid((3, 2), (1, 0), rowspan=1, colspan=1)
+    ax4 = plt.subplot2grid((3, 2), (1, 1), rowspan=1, colspan=1)
+    for kw in all_keywords:
+        ax3.plot(data[kw], label=kw)
+    ax4.bar(x_pos, media, align='center')
+    plt.xticks(x_pos, all_keywords)
+
+    #Last 3-months
+    pytrends.build_payload(all_keywords, cat, timeframes[2], geo, gprop)
+
+    data =  pytrends.interest_over_time()
+    media = data.mean()
+    media = round(media / media.max() * 100, 2)
+    ax5 = plt.subplot2grid((3, 2), (2, 0), rowspan=1, colspan=1)
+    ax6 = plt.subplot2grid((3, 2), (2, 1), rowspan=1, colspan=1)
+    for kw in all_keywords:
+        ax5.plot(data[kw], label=kw)
+    ax6.bar(x_pos, media[0: len(all_keywords)], align='center')
+    plt.xticks(x_pos, all_keywords)
+
+    ax1.set_ylabel('Últimos 5 anos')
+    ax3.set_ylabel('Último ano')
+    ax5.set_ylabel('Últimos 3 meses')
+    ax1.set_title('Interesse por todo tempo', fontsize=14)
+    ax2.set_title('Interesse por período', fontsize=14)
+    ax5.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m'))
+    ax1.legend()
+    ax3.legend()
+    ax5.legend()
+    plt.show()
+
+comparacao()
+
+# SCRIPT FINALIZADO!
